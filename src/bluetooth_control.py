fab63ff1d9544219ebc43145c73b1041f01f7c5d
longdd-pitre <longdd.pitre@gmail.com>
2023-11-28 12:47:04 +0700

    net

--- src/bluetooth_control.py
+++ src/bluetooth_control.py
@@ -5,7 +5,7 @@ import serial
 import time
 
 if __name__ == '__main__':
-    DataSerial = serial.Serial('/dev/cu.HC-06', 9600)
+    DataSerial = serial.Serial('COM10', 9600)
     convert = ConvertSpeechToSignal()
     result = convert.convert()
     print("Tín hiệu truyền cho robot là: ", result)
